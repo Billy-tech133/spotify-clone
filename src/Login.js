663b2a3fd689f4147598052682877b8766df2693
Nyuykisamo Lamfih <lamfihbilly@hotmail.com>
2021-08-16 16:03:49 +0100

    login and auth complete

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Login.css";
+import { loginUrl } from "./spotify";
 function Login() {
   return (
     <div className="login">
       <img src="https://bit.ly/2Un4maP" alt="" />
-      <a>LOGIN WITH SPOTIFY</a>
+      <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
     </div>
   );
 }
--- src/spotify.js
+++ src/spotify.js
@@ -2,13 +2,27 @@
 // documentation/web-playback-sdk/quick-start/#
 
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-const redirectUri = "http://localhost:3000";
-const clientId = "8765545b25344f0095aa282cbadd8093";
+const redirectUri = "http://localhost:3000/";
+const clientId = "1703d763623c4cd082b54ddc72787d85";
 
 const scopes = [
   "user-read-currently-playing",
-  "user-read-recently-playing",
-  "user-read-playback-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
   "user-top-read",
   "user-modify-playback-state",
 ];
+
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+      return initial;
+    }, {});
+};
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
