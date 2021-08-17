49b7a5f6e738b9d7d61228cffcb47dd98cc758c6
Nyuykisamo Lamfih <lamfihbilly@hotmail.com>
2021-08-17 09:17:31 +0100

    authenticatiom redux and login complete

---
+++ src/reducer.js
@@ -0,0 +1,29 @@
+export const initialState = {
+  token:
+    "BQA0MyCtjnIzQwdE2so6m4ksBiCYXsvScp1zTeUOxhCBjWfEh_rDqzM3Dz8t4O_LWLwV5T7UOb9pR1T4fv8JzZe_W3MDuNUbEwZNnfLYo-o1VxlagfVmqR9KBMSE6qjI7zTro-MyrkNn08RRn47Gt7fXD5r746blF7te1Slz7K3Pfh1p",
+  user: null,
+  playlists: [],
+  playing: false,
+  item: null,
+};
+
+const reducer = (state, action) => {
+  //   console.log(action);
+
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+    case "SET_TOKEN":
+      return {
+        ...state,
+        user: action.token,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
